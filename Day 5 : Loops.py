1df4eb0a8eca687c35bf74cb8d0695951f4bb002
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2022-05-02 00:21:19 +0530

    Create Day 5 : Loops.py

---
+++ Day 5 : Loops.py
@@ -0,0 +1,22 @@
+"""
+Objective
+In this challenge, we will use loops to do some math. Check out the Tutorial tab to learn more.
+
+Task
+Given an integer, n, print its first 10 multiples. Each multiple should be printed on a new line in the form: n x i = result.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    c=1
+    for i in range(1,11):
+        print(n,"x",i,"=",i*n)
