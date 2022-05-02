5a0f6d023a53867c400b7d68b4db9fb98d551bd7
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2022-05-03 01:21:08 +0530

    Create Day 7 : Arrays.py

---
+++ Day 7 : Arrays.py
@@ -0,0 +1,10 @@
+#---------------@AbhijeetK1N6---------------------
+
+"""
+Objective:
+Today, we will learn about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task:
+Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+"""
+
