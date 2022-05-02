fdf49f1eea0c1efb0043e519aea0c94a3b37d4d0
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2022-05-03 01:48:29 +0530

    Update Day 7 : Arrays.py

--- Day 7 : Arrays.py
+++ Day 7 : Arrays.py
@@ -8,3 +8,18 @@ Task:
 Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
 """
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#--coded by send--
+if __name__ == '__main__':
+    n=int(input())
+    arr = list(map(int, input().rstrip().split()))
+    arr.reverse()
+    for i in (arr):
+        print(i,end=" ")
